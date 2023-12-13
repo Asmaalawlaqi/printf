b3eae13567e988aa318e65e557a888b7c6a14267
Asmaalawlaqi <143976152+Asmaalawlaqi@users.noreply.github.com>
2023-12-13 15:43:05 +0400

    Update ft_printf_function.c

--- ft_printf/ft_printf_function.c
+++ ft_printf/ft_printf_function.c
@@ -43,7 +43,7 @@ void	ft_putnbr(int nb)
 	if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
-		ft_putchar(nb % 10 + '0');
+		ft_putchar(nb % 10 + '0'); // convertion takes place, turns the int to an ascii
 	}
 	else
 	{
